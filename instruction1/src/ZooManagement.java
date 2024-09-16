8ca775a2f8f5400a2b497a1a25d75ac3a89b190a
Nour <nour.zouaghi@esprit.tn>
2024-09-16 21:17:38 +0100

    ceci est le projet concernant instruction 1

---
+++ instruction1/src/ZooManagement.java
@@ -0,0 +1,11 @@
+
+public class ZooManagement {
+    public static void main(String[] args) {
+        int nbrCages = 20;
+        String zooName = " my zoo ";
+        System.out.printf( zooName + " comporte " + nbrCages );
+
+
+
+    }
+}
