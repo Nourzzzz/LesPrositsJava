b0bfe32b6bb7cc337e0e94a3ca16dd429c5ebf84
Nour <nour.zouaghi@esprit.tn>
2024-09-29 10:06:40 +0100

    ceci est l'instruction 11 du prosit 3, la methode retourne l'indice de animal dans le tableau si il existe sinon -1, et si il ya deux animaux avec le meme nom, elle retourne l'indice du premier

--- Prosinum2/src/Zoo.java
+++ Prosinum2/src/Zoo.java
@@ -35,19 +35,40 @@ public class Zoo {
         System.out.printf("Nombre de cage: " + a.nbrCages);
     }
 
+    void displayAnimals() {
+        for (Animal animal : animals) {
+            System.out.println(animal);
+        }
+
+    }
+
+    int searchAnimal(String name) {
+        for (int i = 0; i < nbrAnimals; i++) {
+            if (animals[i].name.equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
     public String toString() {
         return String.format("Name: %s, City: %s, Nombre de cages: %d", this.name, this.city, this.nbrCages);
     }
 
     public static void main(String[] args) {
-        Animal lion = new Animal("felin", "Black", 20, false);
+        Animal lion = new Animal("felin", "Black1", 20, false);
         Animal otter = new Animal("Mustelids", "ottie", 5, true);
-        Animal lion1 = new Animal("felin", "Black", 20, false);
+        Animal lion1 = new Animal("felin", "Black2", 20, false);
         Zoo myZoo = new Zoo("aaa", "Tunis", 25);
-        myZoo.displayZoo(myZoo);
-        myZoo.ajouterAnimal(lion, 0);
-        System.out.println(myZoo.toString());
+       // myZoo.displayZoo(myZoo);
+       // myZoo.ajouterAnimal(lion, 0);
+       // System.out.println(myZoo.toString());
         myZoo.addAnimal(lion1);
+        myZoo.addAnimal(otter);
+        myZoo.displayAnimals();
+        int d=myZoo.searchAnimal("black1");
+        System.out.println(d);
 
 
     }
