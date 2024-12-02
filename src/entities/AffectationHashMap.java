4ec96b9f2785ce6c358f910a2e0950d5fdb32710
Nour <nour.zouaghi@esprit.tn>
2024-12-02 21:06:27 +0100

    instruction1

---
+++ src/entities/AffectationHashMap.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AffectationHashMap {
+    private Map<Employe, Departement> affectations;
+
+
+    public AffectationHashMap() {
+        this.affectations = new HashMap<>();
+    }
+
+
+}
