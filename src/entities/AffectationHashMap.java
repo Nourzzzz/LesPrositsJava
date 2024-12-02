06999a46e98e9e1251458181eb8350568aefd1e4
Nour <nour.zouaghi@esprit.tn>
2024-12-02 21:08:50 +0100

    instruction2

--- src/entities/AffectationHashMap.java
+++ src/entities/AffectationHashMap.java
@@ -12,5 +12,14 @@ public class AffectationHashMap {
         this.affectations = new HashMap<>();
     }
 
+    public void ajouterEmployeDepartement(Employe e, Departement d) {
+        if (affectations.containsKey(e)) {
+            System.out.println("L'employé " + e.getNom() + " est déjà affecté au département "
+                    + affectations.get(e).getNom());
+        } else {
+            affectations.put(e, d);
+            System.out.println("Employé " + e.getNom() + " affecté au département " + d.getNom());
+        }
+    }
 
 }
